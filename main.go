bdab7cabf8a6bc2c922d57dc5b17d646a6907cf8
owsei <owseiwastaken@gmail.com>
2022-11-11 22:34:33 -0300

    make/save/load pastes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 	"syscall"
 	"time"
 )
@@ -14,35 +15,66 @@ var (
 	codebody string
 	mainbody string
 	err error
+	cyan = RGB(70, 160, 255)
+	nc = RGB(255, 255, 255)
+	green = RGB(0, 255, 0)
+	red = RGB(255, 0, 0)
 )
 
 func GetCodeHandler(w http.ResponseWriter, sc uint32) {
-	pg, err := load(sc)
+	paste, err := load(sc)
 	if err != nil {
 		fprintf(w, "No such Paste %d", sc)
+		printf("%sclient%s didn't get the paste %d\n", red, nc, sc)
 		return
 	}
-	fprintf(w, codebody, string(pg.paste))
+	fprintf(w, codebody, string(paste))
 	//fprintf(w, codebody)
-	printf("client got paste %d\n", sc)
+	printf("%sclient%s got paste %d\n", green, nc, sc)
 }
 
 func HandlerMainPage(w http.ResponseWriter, r *http.Request) {
 	fprintf(w, mainbody)
-	printf("client got the main page\n")
+	printf("%sclient%s got the main page\n", green, nc)
+}
+
+func HandleShowCode(w http.ResponseWriter, code uint32) {
+	fprintf(w, "<html><body><p>code: </p><h1>%d</h1></body></html>", code)
+	printf("%sclient%s notified of new paste's code %d\n", green, nc, code)
+}
+
+func HandleMakePage(w http.ResponseWriter, v string) {
+	vm, err := url.ParseQuery(v)
+	panic(err)
+	paste, ok := vm["paste"]
+	if ok {
+		code := save(paste[0])
+		printf("%sclient%s made a new paste %d\n", green, nc, code)
+		HandleShowCode(w, code)
+	} else {
+		printf("%client%s didn't made the paste\n", red, nc)
+		printf("vars: %v\n", vm)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	printf("client requested %s\n", r.URL.Path)
+	printf("%sclient%s requested %s with %s\n", cyan, nc, r.URL.Path, r.URL.Query())
 	if len(r.URL.Path) == 0 {
 		HandlerMainPage(w, r)
 		return
 	}
+
+	if r.URL.Path == "/make" {
+		HandleMakePage(w, r.URL.RawQuery)
+		return
+	}
+
 	sc, err := strconv.Atoi(r.URL.Path[1:])
 	if err == nil {
 		GetCodeHandler(w, uint32(sc))
 		return
 	}
+	//TODO: make error page
 	HandlerMainPage(w, r)
 }
 
