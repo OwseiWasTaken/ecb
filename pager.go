f08d834a4ef9ebf4e3e77ad5260cb25b8e71ad20
owsei <owseiwastaken@gmail.com>
2022-11-11 22:34:19 -0300

    unpacked Page

--- pager.go
+++ pager.go
@@ -1,8 +1,3 @@
-type Page struct {
-	code uint32
-	paste []byte
-}
-
 func CodeToString (b uint32) (string) {
 	return strconv.Itoa(int(b))
 }
@@ -11,10 +6,17 @@ func CodeToFilename (b uint32) (string) {
 	return "pastes/"+CodeToString(b)+".txt"
 }
 
-func (p *Page) save () {
-	flname := CodeToFilename(p.code)
-	err := os.WriteFile(flname, p.paste, 0644) // 1X 2W 4R
+//TODO: assert unused
+func GetUnusedCode () (uint32) {
+	return uint32(rint(0, int(U32MAX)))
+}
+
+func save (paste string) (uint32) {
+	code := GetUnusedCode()
+	flname := CodeToFilename(code)
+	err := os.WriteFile(flname, []byte(paste), 0644) // 1X 2W 4R
 	panic(err)
+	return code
 }
 
 func GetFileCtime(name string) (ctime int64, err error) {
@@ -27,25 +29,9 @@ func GetFileCtime(name string) (ctime int64, err error) {
 	return ctime, nil
 }
 
-func load(code uint32) (*Page, error) {
+func load(code uint32) (string, error) {
 	flname := CodeToFilename(code)
-	paste, err := os.ReadFile(flname)
-	if err != nil {
-		return &Page{0, []byte("can't find code")}, err
-	}
-	return &Page{code, paste}, err
-}
-
-func GetUnusedCode () (uint32) {
-	return uint32(rint(0, int(U32MAX)))
-}
-
-func MakePage (paste string) (*Page) {
-	code:=GetUnusedCode()
-	return &Page{code, []byte(paste)}
-}
-
-func (p *Page) repr () {
-	printf("code: %d\npaste: %s\n", p.code, string(p.paste))
+	p, err := os.ReadFile(flname)
+	return string(p), err
 }
 
